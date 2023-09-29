22e6599117c19388bb6f242d51117f3167d56d63
Valentin Ackva <valentin.ackva@gmail.com>
2023-09-29 23:44:10 +0200

    adding minimal-libtorch example

--- minimal-inference/libtorch/minimal-libtorch.cpp
+++ minimal-inference/libtorch/minimal-libtorch.cpp
@@ -5,25 +5,29 @@ Licence: modified BSD
 
 ========================================================================== */
 
-// #include <torch/script.h> // One-stop header.
+#include <torch/script.h>
 
 #include <iostream>
 #include <memory>
 
 int main(int argc, const char* argv[]) {
 
-  // const char* filename = "/Users/faresschulz/Desktop/ml_repo/GuitarLSTM/pytorch-version/model.pt";
+    std::string filepath = MODELS_PATH;
+    const std::string modelpath = filepath + "/model.pt";
 
-  // torch::jit::script::Module module;
-  // try {
-  //   // Deserialize the ScriptModule from a file using torch::jit::load().
-  //   module = torch::jit::load(filename);
-  // }
-  // catch (const c10::Error& e) {
-  //   std::cerr << "error loading the model\n";
-  //   return -1;
-  // }
+    std::cout << "Loading model from " << modelpath << std::endl;
 
-  // std::cout << "ok\n";
-  return 0;
+    torch::jit::script::Module module;
+    try {
+     // Deserialize the ScriptModule from a file using torch::jit::load().
+        module = torch::jit::load(modelpath);
+    }
+    catch (const c10::Error& e) {
+        std::cerr << "error loading the model\n";
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+
+    // std::cout << "ok\n";
+    return 0;
 }
