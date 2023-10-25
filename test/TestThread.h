051971dc926da5d1fb05b5f414e78966b2b2a279
Valentin Ackva <valentin.ackva@gmail.com>
2023-10-25 23:31:08 +0200

    added juce::Thread benchmarking

---
+++ test/TestThread.h
@@ -0,0 +1,23 @@
+//
+// Created by valentin.ackva on 25.10.2023.
+//
+
+#ifndef NN_INFERENCE_TEMPLATE_TESTTHREAD_H
+#define NN_INFERENCE_TEMPLATE_TESTTHREAD_H
+
+#include <JuceHeader.h>
+
+class TestThread : public juce::Thread {
+public:
+    TestThread() : juce::Thread("TestThread") { }
+
+    ~TestThread() {
+        stopThread(100);
+    }
+
+    void run() override {
+    }
+
+};
+
+#endif //NN_INFERENCE_TEMPLATE_TESTTHREAD_H
--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 #include "../source/PluginProcessor.h"
 #include "../source/PluginEditor.h"
+#include "TestThread.h"
 
 static void BM_PROCESSOR(benchmark::State& state) {
     auto gui = juce::ScopedJuceInitialiser_GUI {};
@@ -65,9 +66,42 @@ BENCHMARK_DEFINE_F(InferenceFixture, BM_TFLITE_INFERENCE)(benchmark::State& stat
     }
 }
 
+
+// --- Benchmarking threads ---
+
+class ThreadFixture : public benchmark::Fixture
+{
+public:
+    std::unique_ptr<TestThread> thread;
+
+    ThreadFixture() { }
+
+    void SetUp(const ::benchmark::State& state) {
+        thread = std::make_unique<TestThread>();
+    }
+
+    void TearDown(const ::benchmark::State& state) {
+        thread.reset();
+    }
+};
+
+
+BENCHMARK_DEFINE_F(ThreadFixture, BM_THREAD)(benchmark::State& state) {
+    for (auto _ : state) {
+        thread->startThread(juce::Thread::Priority::highest);
+
+        while (thread->isThreadRunning()) {
+            std::this_thread::sleep_for(std::chrono::nanoseconds (10));
+        }
+    }
+}
+
+
+
 // Register the function as a benchmark
 BENCHMARK(BM_PROCESSOR)->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_EDITOR)->Unit(benchmark::kMillisecond);
 BENCHMARK_REGISTER_F(InferenceFixture, BM_ONNX_INFERENCE)->Unit(benchmark::kMillisecond)->Iterations(32)->Repetitions(1)->RangeMultiplier(2)->Range(128, 8<<10);
 BENCHMARK_REGISTER_F(InferenceFixture, BM_LIBTORCH_INFERENCE)->Unit(benchmark::kMillisecond)->Iterations(32)->Repetitions(1)->RangeMultiplier(2)->Range(128, 8<<10);
 BENCHMARK_REGISTER_F(InferenceFixture, BM_TFLITE_INFERENCE)->Unit(benchmark::kMillisecond)->Iterations(32)->Repetitions(1)->RangeMultiplier(2)->Range(128, 8<<10);
+BENCHMARK_REGISTER_F(ThreadFixture, BM_THREAD)->Unit(benchmark::kMillisecond)->Iterations(32)->Repetitions(1);
