558d41681071d2fa8d48f6f0193e0bbfd59bcdae
Valentin Ackva <valentin.ackva@gmail.com>
2023-10-27 16:36:25 +0200

    added dry/wet mixer into processing

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -95,6 +95,8 @@ void AudioPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPer
                                  static_cast<juce::uint32>(samplesPerBlock),
                                  static_cast<juce::uint32>(getTotalNumInputChannels())};
     inferenceManager.prepareToPlay(spec);
+    dryWetMixer.prepare(spec);
+
     juce::ignoreUnused (sampleRate, samplesPerBlock);
 }
 
@@ -137,28 +139,15 @@ void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    inferenceManager.processBlock(buffer);
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
-    }
+    float currentMix = parameters.getRawParameterValue("mix")->load();
+    dryWetMixer.setDryWetProportion(currentMix);
+
+    dryWetMixer.setDrySamples(buffer);
+    inferenceManager.processBlock(buffer);
+    dryWetMixer.setWetSamples(buffer);
 }
 
 //==============================================================================
@@ -169,7 +158,8 @@ bool AudioPluginAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
 {
-    return new AudioPluginAudioProcessorEditor (*this);
+//    return new AudioPluginAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
