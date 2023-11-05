74b67e7c55a4db3f902d0897d5d634ae4aa082a5
Fares Schulz <schulzfares@gmail.com>
2023-11-05 14:27:04 +0100

    implemented percentile calc; clean up

--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -6,12 +6,53 @@
 // TODO Make sure that benchmarks also work when HOST_BUFFER_SIZE % MODEL_INPUT_SIZE != 0
 
 /* ============================================================ *
+ * ========================= Configs ========================== *
+ * ============================================================ */
+
+#define PERCENTILE 0.999
+
+/* ============================================================ *
  * ===================== Helper functions ===================== *
  * ============================================================ */
 
 static float randomSample () {
     return -1.f + (float) (std::rand()) / ((float) (RAND_MAX/2.f));
 }
+
+static double calculatePercentile(const std::vector<double>& v, double percentile) {
+    // Make sure the data is not empty
+    if (v.empty()) {
+        throw std::invalid_argument("Input vector is empty.");
+    }
+
+    // Sort the data in ascending order
+    std::vector<double> sortedData = v;
+    std::sort(sortedData.begin(), sortedData.end());
+
+    // Calculate the index for the 99th percentile
+    size_t n = sortedData.size();
+    size_t percentileIndex = (size_t) (percentile * (n - 1));
+
+    // Check if the index is an integer
+    if (percentileIndex == static_cast<size_t>(percentileIndex)) {
+        // The index is an integer, return the value at that index
+        return sortedData[static_cast<size_t>(percentileIndex)];
+    } else {
+        // Interpolate between the two nearest values
+        size_t lowerIndex = static_cast<size_t>(percentileIndex);
+        size_t upperIndex = lowerIndex + 1;
+        double fraction = percentileIndex - lowerIndex;
+        return (1.0 - fraction) * sortedData[lowerIndex] + fraction * sortedData[upperIndex];
+    }
+}
+
+const auto calculateMin = [](const std::vector<double>& v) -> double {
+    return *(std::min_element(std::begin(v), std::end(v)));
+};
+
+const auto calculateMax = [](const std::vector<double>& v) -> double {
+    return *(std::max_element(std::begin(v), std::end(v)));
+};
 
 /* ============================================================ *
  * =================== PROCESSOR AND EDITOR =================== *
@@ -217,47 +258,41 @@ BENCHMARK(BM_EDITOR)->Unit(benchmark::kMillisecond);
 //
 BENCHMARK_REGISTER_F(ProcessBlockFixture, BM_ONNX_BACKEND)
 ->Unit(benchmark::kMillisecond)
-->Iterations(1)->Repetitions(32)
+->Iterations(1)->Repetitions(5000)
 ->RangeMultiplier(2)->Range(128, 8<<10)
-->ComputeStatistics("min", [](const std::vector<double>& v) -> double {
-    return *(std::min_element(std::begin(v), std::end(v)));
-  })
-->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
-    return *(std::max_element(std::begin(v), std::end(v)));
+->ComputeStatistics("min", calculateMin)
+->ComputeStatistics("max", calculateMax)
+->ComputeStatistics("percentile", [](const std::vector<double>& v) -> double {
+    return calculatePercentile(v, PERCENTILE);
   })
 ->DisplayAggregatesOnly(true);
 
 BENCHMARK_REGISTER_F(ProcessBlockFixture, BM_LIBTORCH_BACKEND)
 ->Unit(benchmark::kMillisecond)
-->Iterations(1)->Repetitions(32)
+->Iterations(1)->Repetitions(5000)
 ->RangeMultiplier(2)->Range(128, 8<<10)
-->ComputeStatistics("min", [](const std::vector<double>& v) -> double {
-    return *(std::min_element(std::begin(v), std::end(v)));
-  })
-->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
-    return *(std::max_element(std::begin(v), std::end(v)));
+->ComputeStatistics("min", calculateMin)
+->ComputeStatistics("max", calculateMax)
+->ComputeStatistics("percentile", [](const std::vector<double>& v) -> double {
+    return calculatePercentile(v, PERCENTILE);
   })
 ->DisplayAggregatesOnly(true);
 
 BENCHMARK_REGISTER_F(ProcessBlockFixture, BM_TFLITE_BACKEND)
 ->Unit(benchmark::kMillisecond)
-->Iterations(1)->Repetitions(32)
+->Iterations(1)->Repetitions(5000)
 ->RangeMultiplier(2)->Range(128, 8<<10)
-->ComputeStatistics("min", [](const std::vector<double>& v) -> double {
-    return *(std::min_element(std::begin(v), std::end(v)));
-  })
-->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
-    return *(std::max_element(std::begin(v), std::end(v)));
+->ComputeStatistics("min", calculateMin)
+->ComputeStatistics("max", calculateMax)
+->ComputeStatistics("percentile", [](const std::vector<double>& v) -> double {
+    return calculatePercentile(v, PERCENTILE);
   })
 ->DisplayAggregatesOnly(true);
 
 // BENCHMARK_REGISTER_F(ThreadFixture, BM_THREAD)
-// ->Unit(benchmark::kMillisecond)
-// ->Iterations(1)->Repetitions(128)
-// ->ComputeStatistics("min", [](const std::vector<double>& v) -> double {
-//     return *(std::min_element(std::begin(v), std::end(v)));
-//   })
-// ->ComputeStatistics("max", [](const std::vector<double>& v) -> double {
-//     return *(std::max_element(std::begin(v), std::end(v)));
-//   })
+// ->Iterations(1)->Repetitions(1000)
+// ->RangeMultiplier(2)->Range(128, 8<<10)
+// ->ComputeStatistics("min", calculateMin)
+// ->ComputeStatistics("max", calculateMax)
+// ->ComputeStatistics("99_percentile", calculatePercentile)
 // ->DisplayAggregatesOnly(true);
