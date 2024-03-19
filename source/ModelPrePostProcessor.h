0907a91bad9d4ef2429570a231528e1b0845782b
Fares Schulz <schulzfares@gmail.com>
2024-03-20 00:21:15 +0100

    Added model config and prepostprocessor for GuitarLSTM model

---
+++ source/ModelPrePostProcessor.h
@@ -0,0 +1,20 @@
+#ifndef ANIRA_HYBRIDNNPREPOSTPROCESSOR_H
+#define ANIRA_HYBRIDNNPREPOSTPROCESSOR_H
+
+#include "ModelConfig.h"
+#include <anira/anira.h>
+
+class HybridNNPrePostProcessor : public anira::PrePostProcessor
+{
+public:
+    virtual void preProcess(anira::RingBuffer& input, anira::AudioBufferF& output, [[maybe_unused]] anira::InferenceBackend currentInferenceBackend) override {
+        for (size_t batch = 0; batch < config.m_batch_size; batch++) {
+            size_t baseIdx = batch * config.m_model_input_size_backend;
+            popSamplesFromBuffer(input, output, config.m_model_input_size, config.m_model_input_size_backend-config.m_model_input_size, baseIdx);
+        }
+    };
+    
+    anira::InferenceConfig config = hybridNNConfig;
+};
+
+#endif //ANIRA_HYBRIDNNPREPOSTPROCESSOR_H
