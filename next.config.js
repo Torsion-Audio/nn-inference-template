0e5dfb564f44d41f946fcd8bdfdbfe31f0ff2426
Valentin Ackva <valentin.ackva@insone.de>
2024-03-22 17:45:39 +0100

    adjusted github pages subpath

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
   },
 
   // Set the base path and asset prefix to match the GitHub Pages subpath
-  basePath: '/nn-inference-template/',
-  assetPrefix: '/nn-inference-template/',
+  basePath: '/nn-inference-template/tree/docs/',
+  assetPrefix: '/nn-inference-template/tree/docs/',
 
   // Optional configurations:
   // Enable trailing slashes in URLs. 
