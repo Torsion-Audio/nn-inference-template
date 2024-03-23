8bfbdf4d51d771bad68d8770017f8cbfb1b1d36c
Valentin Ackva <valentin.ackva@insone.de>
2024-03-23 18:25:28 +0100

    added iframes

--- _posts/about-us/fares.md
+++ _posts/about-us/fares.md
@@ -2,7 +2,7 @@
 title: "Fares Schulz"
 excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus."
 coverImage: "/nn-inference-template/assets/blog/authors/fares.png"
-date: "2020-03-16T05:35:07.322Z"
+date: "2020-03-16T05:38:07.322Z"
 author:
   name: Fares Schulz
   picture: "/nn-inference-template/assets/blog/authors/jj.jpeg"
--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -8,9 +8,18 @@ export function PostBody({ content }: Props) {
   return (
     <div className="max-w-2xl mx-auto">
       <div
-        className={markdownStyles["markdown"]}
+        className="markdown"
         dangerouslySetInnerHTML={{ __html: content }}
       />
+      <iframe 
+        src="https://docs.google.com/presentation/d/e/2PACX-1vQ8kVxwsa4YRc9jeCdajiDaRJzmkpLL-hPzVpSNipetQP90AIdL2MzPdCkpcTrnI4sSkV076l93gQkA/embed?start=false&loop=false&delayms=3000" 
+        frameBorder="0" 
+        width="1440" 
+        height="839" 
+        allowFullScreen={true} 
+        mozallowfullscreen="true" 
+        webkitallowfullscreen="true">
+      </iframe>
     </div>
   );
 }
