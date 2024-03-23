c31f785bd6d5a856800d0d34fde36df7b1fe6ceb
Fares Schulz <schulzfares@gmail.com>
2024-03-23 17:07:08 +0100

    benchmark unit tests deleted; use aniras benchmark examples instead

--- test/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <benchmark/benchmark.h>
-#include <gtest/gtest.h>
-
-TEST(Benchmark, All){
-    benchmark::RunSpecifiedBenchmarks();
-    std::cout << "--------------------------------------------------------------" << std::endl;
-}
