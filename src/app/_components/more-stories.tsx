ef1117bf3ce13d02d3b4040889ccb615af6c6c1e
Valentin Ackva <74772523+vackva@users.noreply.github.com>
2024-03-24 17:19:41 +0100

    Update more-stories.tsx

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -37,7 +37,7 @@ export function MoreStories({ posts }: Props) {
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <PostPreview
-            key={post.slug}
+            key={modifySlug(post.slug, post.title)}
             title={post.title}
             coverImage={post.coverImage}
             date={post.date}
