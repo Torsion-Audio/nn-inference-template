8fb38412c10d28b20c86d0ce30b25c2150644bb0
Fares Schulz <schulzfares@gmail.com>
2024-03-27 22:14:30 +0100

    Updated source code to comply with anira v0.0.2

--- source/ModelConfig.h
+++ source/ModelConfig.h
@@ -1,35 +1,32 @@
-#ifndef ANIRA_HYBRIDNNCONFIG_H
-#define ANIRA_HYBRIDNNCONFIG_H
+#ifndef NN_INFERENCE_TEMPLATE_MODELCONFIG_H
+#define NN_INFERENCE_TEMPLATE_MODELCONFIG_H
 
 #include <anira/anira.h>
-
-#define HYBRIDNN_MAX_INFERENCE_TIME 2048
 
 static anira::InferenceConfig hybridNNConfig(
 #ifdef USE_LIBTORCH
         GUITARLSTM_MODELS_PATH_PYTORCH + std::string("model_0/GuitarLSTM-dynamic.pt"),
-        {2048, 1, 150},
-        {2048, 1},
+        {256, 1, 150},
+        {256, 1},
 #endif
 #ifdef USE_ONNXRUNTIME
         GUITARLSTM_MODELS_PATH_PYTORCH + std::string("model_0/GuitarLSTM-libtorch-dynamic.onnx"),
-        {2048, 1, 150},
-        {2048, 1},
+        {256, 1, 150},
+        {256, 1},
 #endif
 #ifdef USE_TFLITE
-        GUITARLSTM_MODELS_PATH_TENSORFLOW + std::string("model_0/GuitarLSTM-2048.tflite"),
-        {2048, 150, 1},
-        {2048, 1},
+        GUITARLSTM_MODELS_PATH_TENSORFLOW + std::string("model_0/GuitarLSTM-256.tflite"),
+        {256, 150, 1},
+        {256, 1},
 #endif
-        2048,
-        1,
+        256,
         150,
         1,
-        HYBRIDNN_MAX_INFERENCE_TIME,
+        5.33f,
         0,
         false,
         0.5f,
         false
 );
 
-#endif //ANIRA_HYBRIDNNCONFIG_H
+#endif //NN_INFERENCE_TEMPLATE_MODELCONFIG_H
--- source/ModelPrePostProcessor.h
+++ source/ModelPrePostProcessor.h
@@ -1,5 +1,5 @@
-#ifndef ANIRA_HYBRIDNNPREPOSTPROCESSOR_H
-#define ANIRA_HYBRIDNNPREPOSTPROCESSOR_H
+#ifndef NN_INFERENCE_TEMPLATE_MODELPREPOSTPROCESSOR_H
+#define NN_INFERENCE_TEMPLATE_MODELPREPOSTPROCESSOR_H
 
 #include "ModelConfig.h"
 #include <anira/anira.h>
@@ -9,12 +9,12 @@ class HybridNNPrePostProcessor : public anira::PrePostProcessor
 public:
     virtual void preProcess(anira::RingBuffer& input, anira::AudioBufferF& output, [[maybe_unused]] anira::InferenceBackend currentInferenceBackend) override {
         for (size_t batch = 0; batch < config.m_batch_size; batch++) {
-            size_t baseIdx = batch * config.m_model_input_size_backend;
-            popSamplesFromBuffer(input, output, config.m_model_input_size, config.m_model_input_size_backend-config.m_model_input_size, baseIdx);
+            size_t baseIdx = batch * config.m_model_input_size;
+            popSamplesFromBuffer(input, output, config.m_model_output_size, config.m_model_input_size-config.m_model_output_size, baseIdx);
         }
     };
     
     anira::InferenceConfig config = hybridNNConfig;
 };
 
-#endif //ANIRA_HYBRIDNNPREPOSTPROCESSOR_H
+#endif //NN_INFERENCE_TEMPLATE_MODELPREPOSTPROCESSOR_H
