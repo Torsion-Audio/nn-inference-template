333a6e2d764000e7607d353616e2c8773f14f9aa
Fares Schulz <schulzfares@gmail.com>
2024-03-28 23:42:22 +0100

    Updated rpath for macOS

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -6,29 +6,36 @@
 include(GNUInstallDirs)
 
 # define the dircetory where the library will be installed CMAKE_INSTALL_PREFIX
-if(DEFINED CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-    message( STATUS "CMAKE_INSTALL_PREFIX will be set to ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}-${PROJECT_VERSION}" )
-    set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}-${PROJECT_VERSION}" CACHE PATH "Where the library will be installed to" FORCE)
-else()
-    message(STATUS "CMAKE_INSTALL_PREFIX was already set to ${CMAKE_INSTALL_PREFIX}")
-endif()
+set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}-${PROJECT_VERSION}" CACHE PATH "Where the library will be installed to" FORCE)
 
 # at install the rpath is cleared by default so we have to set it again for the installed shared library to find the other libraries
 # in this case we set the rpath to the directories where the other libraries are installed
 # $ORIGIN is a special token that gets replaced by the directory of the library at runtime from that point we could navigate to the other libraries
 # it is a little strange but the onnxruntime library and libtorch work without this setting in the JUCE example... but tensorflowlite does not
 
-set_target_properties(${TARGET_NAME}_Standalone
+if(LINUX)
+    set_target_properties(${TARGET_NAME}_Standalone
+        PROPERTIES
+            INSTALL_RPATH "$ORIGIN/lib"
+    )
+    set_target_properties(${TARGET_NAME}_VST3
+        PROPERTIES
+            INSTALL_RPATH "$ORIGIN/../../../lib"
+    )
+elseif(APPLE)
+    set_target_properties(${TARGET_NAME}_Standalone
     PROPERTIES
-        INSTALL_RPATH "$ORIGIN/lib"
-        DEBUG_POSTFIX "d"
-)
-
-set_target_properties(${TARGET_NAME}_VST3
+        INSTALL_RPATH "@loader_path/../../../lib;/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64"
+    )
+    set_target_properties(${TARGET_NAME}_VST3
     PROPERTIES
-        INSTALL_RPATH "$ORIGIN/../../../lib"
-        DEBUG_POSTFIX "d"
-)
+        INSTALL_RPATH "@loader_path/../../../lib;/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64"
+    )
+    set_target_properties(${TARGET_NAME}_AU
+    PROPERTIES
+        INSTALL_RPATH "@loader_path/../../../lib;/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64"
+    )
+endif()
 
 # install the target and create export-set
 install(TARGETS ${TARGET_NAME}_Standalone ${TARGET_NAME}_VST3
@@ -42,7 +49,6 @@ if(APPLE)
     # install the target and create export-set
     install(TARGETS ${TARGET_NAME}_AU
         # these get default values from GNUInstallDirs
-        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
     )
 endif()
