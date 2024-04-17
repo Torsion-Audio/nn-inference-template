827403714b5c8930a765afa5d9040798567f0a40
Fares Schulz <schulzfares@gmail.com>
2024-04-17 14:41:13 +0200

    New handling of libtorch dependencies; added libomp to rpath on apple silicon

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -24,41 +24,42 @@ endif()
 # $ORIGIN in Linux is a special token that gets replaced by the directory of the library at runtime from that point we could navigate to the other libraries
 # The same token for macOS is @loader_path
 if(LINUX)
-    set_target_properties(${TARGET_NAME}_Standalone
-        PROPERTIES
-            INSTALL_RPATH "$ORIGIN/../lib"
-    )
-    set_target_properties(${TARGET_NAME}_VST3
-        PROPERTIES
-            INSTALL_RPATH "$ORIGIN/../lib"
-    )
+    set(INSTALL_TARGETS ${TARGET_NAME}_Standalone ${TARGET_NAME}_VST3)
+    foreach(TARGET ${INSTALL_TARGETS})
+        set_target_properties(${TARGET}
+            PROPERTIES
+                INSTALL_RPATH "$ORIGIN/../lib"
+        )
+    endforeach()
 elseif(APPLE)
-    set_target_properties(${TARGET_NAME}_Standalone
-    PROPERTIES
-        INSTALL_RPATH "@loader_path/../lib;/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64"
-    )
-    set_target_properties(${TARGET_NAME}_VST3
-    PROPERTIES
-        INSTALL_RPATH "@loader_path/../lib;/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64"
-    )
-    set_target_properties(${TARGET_NAME}_AU
-    PROPERTIES
-        INSTALL_RPATH "@loader_path/../lib;/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64"
-    )
+    set(OSX_RPATHS "@loader_path/../lib;@loader_path/../../../../lib;@loader_path/../../../")
+    set(INSTALL_TARGETS ${TARGET_NAME}_Standalone ${TARGET_NAME}_VST3 ${TARGET_NAME}_AU)
+    if (CMAKE_OSX_ARCHITECTURES STREQUAL "x86_64")
+        list(APPEND OSX_RPATHS "/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64")
+    elseif (CMAKE_OSX_ARCHITECTURES STREQUAL "arm64")
+        list(APPEND OSX_RPATHS "/opt/homebrew/opt/libomp/lib")
+    else()
+        if (CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
+            list(APPEND OSX_RPATHS "/opt/intel/oneapi/mkl/2023.0.0/lib;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64;/opt/intel/oneapi/mkl/2023.0.0/lib/intel64_win;/opt/intel/oneapi/mkl/2023.0.0/lib/win-x64")
+        elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm64")
+            list(APPEND OSX_RPATHS "/opt/homebrew/opt/libomp/lib")
+        else()
+            message(FATAL_ERROR "CMAKE_OSX_ARCHITECTURES and CMAKE_HOST_SYSTEM_PROCESSOR not defined.")
+        endif()
+    endif()
+    foreach(TARGET ${INSTALL_TARGETS})
+        set_target_properties(${TARGET}
+            PROPERTIES
+                INSTALL_RPATH "${OSX_RPATHS}"
+        )
+    endforeach()
 endif()
 
+message(${INSTALL_TARGETS})
 # install the target and create export-set
-install(TARGETS ${TARGET_NAME}_Standalone ${TARGET_NAME}_VST3
+install(TARGETS ${INSTALL_TARGETS}
     # these get default values from GNUInstallDirs
     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
     BUNDLE DESTINATION ${CMAKE_INSTALL_BINDIR}
 )
-
-if(APPLE)
-    # install the target and create export-set
-    install(TARGETS ${TARGET_NAME}_AU
-        # these get default values from GNUInstallDirs
-        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-    )
-endif()
